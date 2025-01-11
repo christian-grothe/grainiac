2f17462dc380fedbb3f5de4c1e302181a848b297
Christian Grothe <christian.grothe@posteo.de>
2025-01-11 18:21:49 +0100

    100 bars

--- grainiac_core/src/lib.rs
+++ grainiac_core/src/lib.rs
@@ -6,7 +6,7 @@ pub mod voice;
 
 const VOICE_NUM: usize = 16;
 pub const INSTANCE_NUM: usize = 4;
-pub const BAR_NUM: usize = 128;
+pub const BAR_NUM: usize = 100;
 
 #[derive(Clone)]
 pub struct DrawData {
